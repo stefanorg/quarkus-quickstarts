18b605447cac1a22ca896c65e7468b0ed639466a
Stefano Corallo <scorallo@argosoft.it>
2020-04-30 12:08:38 +0200

    reproduce error when setting finalName and `quarkus.package.output-name`

---
+++ getting-started/src/main/java/org/acme/getting/started/unused/Unused.java
@@ -0,0 +1,15 @@
+package org.acme.getting.started.unused;
+
+public class Unused {
+
+    public String aMethod() {
+        return "hello";
+    }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return super.toString();
+    }
+
+}
